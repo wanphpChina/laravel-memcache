a503eba15276effc25ca87edcc3e7911c6d697c0
xXKeyleXx <keyle@t-online.de>
2016-01-11 17:17:44 +0100

    Laravel 5.2 update

--- src/MemcacheStore.php
+++ src/MemcacheStore.php
@@ -46,6 +46,23 @@ class MemcacheStore extends TaggableStore implements Store {
 	}
 
 	/**
+	 * Retrieve multiple items from the cache by key.
+	 *
+	 * Items not found in the cache will have a null value.
+	 *
+	 * @param  array $keys
+	 * @return array
+	 */
+	public function many(array $keys)
+	{
+		$return = [];
+		foreach ($keys as $key) {
+			$return[$key] = $this->get($key);
+		}
+		return $return;
+	}
+
+	/**
 	 * Store an item in the cache for a given number of minutes.
 	 *
 	 * @param  string  $key
@@ -56,6 +73,20 @@ class MemcacheStore extends TaggableStore implements Store {
 	public function put($key, $value, $minutes)
 	{
 		$this->memcache->set($this->prefix.$key, $value, false, $minutes * 60);
+	}
+
+	/**
+	 * Store multiple items in the cache for a given number of minutes.
+	 *
+	 * @param  array $values
+	 * @param  int $minutes
+	 * @return void
+	 */
+	public function putMany(array $values, $minutes)
+	{
+		foreach ($values as $key => $value) {
+			$this->put($key, $value, $minutes);
+		}
 	}
 
     /**
